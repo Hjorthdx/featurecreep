82dd124757652d5aa96df55c4896056d1e60c29f
Hjorth <sboels18@student.aau.dk>
2022-07-05 22:53:12 +0200

    Showing all pomodoros in db currently and updating the list by calling invalidateQueries on the client, when a mutation is successful. This refetches the data.

--- src/pages/pomodoro.tsx
+++ src/pages/pomodoro.tsx
@@ -1,6 +1,13 @@
 import { trpc } from '../utils/trpc';
 function Pomodoro() {
-    const { mutate } = trpc.useMutation(['pomodoro.startPomodoro']);
+    const client = trpc.useContext();
+    const { mutate } = trpc.useMutation('pomodoro.startPomodoro', {
+        onSuccess(data, variables, context) {
+            console.log('onSuccess', data, variables, context);
+            client.invalidateQueries(['pomodoro.getAll']);
+        },
+    });
+    const { data: pomodoros } = trpc.useQuery(['pomodoro.getAll']);
     return (
         <div>
             <h1>Pomodoro</h1>
@@ -15,6 +22,10 @@ function Pomodoro() {
             >
                 Add new Pomodoro (25/5)
             </button>
+            {pomodoros &&
+                pomodoros.map((pomodoro, index) => {
+                    return <div key={index}>{pomodoro.createdAt.toLocaleString()}</div>;
+                })}
         </div>
     );
 }
