d6dfa222f4b8fbabec263d05e97cb80887bdd056
Hjorth <sboels18@student.aau.dk>
2022-07-05 22:14:32 +0200

    First play around with session. Only show content on page after being logged in

--- src/pages/api/restricted.ts
+++ src/pages/api/restricted.ts
@@ -1,20 +1,19 @@
 // Example of a restricted endpoint that only authenticated users can access from https://next-auth.js.org/getting-started/example
 
-import { NextApiRequest, NextApiResponse } from "next";
-import { unstable_getServerSession as getServerSession } from "next-auth";
-import { authOptions as nextAuthOptions } from "./auth/[...nextauth]";
+import { NextApiRequest, NextApiResponse } from 'next';
+import { unstable_getServerSession as getServerSession } from 'next-auth';
+import { authOptions as nextAuthOptions } from './auth/[...nextauth]';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const session = await getServerSession(req, res, nextAuthOptions);
+    const session = await getServerSession(req, res, nextAuthOptions);
 
-  if (session) {
-    res.send({
-      content:
-        "This is protected content. You can access this content because you are signed in.",
-    });
-  } else {
-    res.send({
-      error: "You must be sign in to view the protected content on this page.",
-    });
-  }
+    if (session) {
+        res.send({
+            content: 'This is protected content. You can access this content because you are signed in.',
+        });
+    } else {
+        res.send({
+            terror: 'You must be sign in to view he protected content on this page.',
+        });
+    }
 };
