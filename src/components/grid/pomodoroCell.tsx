b5993eaeca6e59bb298a6af42ed1e611fe3e76d5
Hjorth <sboels18@student.aau.dk>
2022-07-06 00:12:15 +0200

    Accidently used a tag to route to /pomodoro instead of Link from next/link.

--- src/components/grid/pomodoroCell.tsx
+++ src/components/grid/pomodoroCell.tsx
@@ -1,9 +1,10 @@
+import Link from 'next/link';
 import GridCell from './gridCell';
 
 function PomodoroCell() {
     return (
         <GridCell>
-            <a href='/pomodoro'>
+            <Link href='/pomodoro'>
                 <div className='p-5 py-10 flex flex-col justify-center h-full text-center'>
                     <h2 className='mb-5 text-3xl'>Pomodoro</h2>
                     <p className='mb-5'>Stay focused and finish tasks effectively.</p>
@@ -11,7 +12,7 @@ function PomodoroCell() {
                         Learn more
                     </button>
                 </div>
-            </a>
+            </Link>
         </GridCell>
     );
 }
