23894b3f70a2584493993a5770a948bd722f9b33
Hjorth <sboels18@student.aau.dk>
2022-07-07 00:44:51 +0200

    Ocd thing had to rearrange the providers alphabetically

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -3,8 +3,8 @@ import CredentialsProvider from 'next-auth/providers/credentials';
 import DiscordProvider from 'next-auth/providers/discord';
 import EmailProvider from 'next-auth/providers/email';
 import FacebookProvider from 'next-auth/providers/facebook';
-import GoogleProvider from 'next-auth/providers/google';
 import GithubProvider from 'next-auth/providers/github';
+import GoogleProvider from 'next-auth/providers/google';
 import RedditProvider from 'next-auth/providers/reddit';
 import TwitchProvider from 'next-auth/providers/twitch';
 import TwitterProvider from 'next-auth/providers/twitter';
@@ -25,13 +25,13 @@ export const authOptions: NextAuthOptions = {
             clientId: process.env.FACEBOOK_ID ?? '',
             clientSecret: process.env.FACEBOOK_SECRET ?? '',
         }),
-        GoogleProvider({
-            clientId: process.env.GOOGLE_ID ?? '',
-            clientSecret: process.env.GOOGLE_SECRET ?? '',
-        }),
         GithubProvider({
             clientId: process.env.GITHUB_ID,
             clientSecret: process.env.GITHUB_SECRET,
+        }),
+        GoogleProvider({
+            clientId: process.env.GOOGLE_ID ?? '',
+            clientSecret: process.env.GOOGLE_SECRET ?? '',
         }),
         RedditProvider({
             clientId: process.env.REDDIT_ID,
