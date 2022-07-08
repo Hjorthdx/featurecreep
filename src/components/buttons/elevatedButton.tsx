535a9cefd764a21f76a6d45b1195d147de39b390
Hjorth <sboels18@student.aau.dk>
2022-07-08 10:48:22 +0200

    Now enables the currently selected mode in the top bar on the pomodoro page

--- src/components/buttons/elevatedButton.tsx
+++ src/components/buttons/elevatedButton.tsx
@@ -1,12 +1,15 @@
 interface Props {
     onClick: () => void;
+    enabled?: boolean;
     children: React.ReactNode;
 }
 
-function ElevatedButton({ onClick, children }: Props) {
+function ElevatedButton({ onClick, enabled, children }: Props) {
     return (
         <button
-            className='m-2 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-2 border border-gray-400 rounded shadow inline-flex items-center'
+            className={`m-2 ${
+                enabled ? 'bg-gray-300' : 'bg-white'
+            } hover:bg-gray-100 text-gray-800 font-semibold py-2 px-2 border border-gray-400 rounded shadow inline-flex items-center`}
             onClick={onClick}
         >
             {children}
--- src/components/pomodoro/topBar.tsx
+++ src/components/pomodoro/topBar.tsx
@@ -1,35 +1,73 @@
+import { useReducer } from 'react';
 import ElevatedButton from '../buttons/elevatedButton';
 import SettingsButton from '../buttons/settingsButton';
 import { PomodoroModes } from './pomodoroModes';
+
+// Could this be done in a nicer way? Currently just setting all booleans everytime a button is clicked.
+
+interface PomodoroMethodAction {
+    type: PomodoroModes;
+}
+
+interface TopbarState {
+    workBool: boolean;
+    breakBool: boolean;
+    longBreakBool: boolean;
+}
+
+const initialState: TopbarState = { workBool: true, breakBool: false, longBreakBool: false };
+
+function pomodoroMethodReducer(state: TopbarState, action: PomodoroMethodAction): TopbarState {
+    switch (action.type) {
+        case 'work':
+            return { ...state, workBool: true, breakBool: false, longBreakBool: false };
+        case 'break':
+            return { ...state, workBool: false, breakBool: true, longBreakBool: false };
+        case 'longBreak':
+            return { ...state, workBool: false, breakBool: false, longBreakBool: true };
+        default:
+            return state;
+    }
+}
 
 interface Props {
     onClick: (mode: PomodoroModes) => void;
 }
 
 function Topbar({ onClick }: Props) {
+    const [state, dispatch] = useReducer(pomodoroMethodReducer, initialState);
+
+    function onPress(mode: PomodoroModes) {
+        dispatch({ type: mode });
+        onClick(mode);
+    }
+
     return (
         <div className='p-5 py-5 inline-flex'>
             <SettingsButton onClick={() => console.log('Settings button!')} />
             <ElevatedButton
+                enabled={state.workBool}
                 onClick={() => {
                     console.log('Work button!');
-                    onClick('work');
+                    onPress('work');
                 }}
             >
                 Work button
             </ElevatedButton>
             <ElevatedButton
+                enabled={state.breakBool}
                 onClick={() => {
                     console.log('Break button!');
-                    onClick('break');
+                    onPress('break');
                 }}
             >
                 Break button
             </ElevatedButton>
             <ElevatedButton
+                enabled={state.longBreakBool}
                 onClick={() => {
                     console.log('Long break button!');
-                    onClick('longBreak');
+                    onPress('longBreak');
                 }}
             >
                 Long break button
