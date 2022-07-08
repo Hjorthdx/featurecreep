d9688248743879417a35742df54f7dae355a2606
Hjorth <sboels18@student.aau.dk>
2022-07-08 11:04:24 +0200

    Removed hover from the currently enabled button

--- src/components/buttons/elevatedButton.tsx
+++ src/components/buttons/elevatedButton.tsx
@@ -8,8 +8,8 @@ function ElevatedButton({ onClick, enabled, children }: Props) {
     return (
         <button
             className={`m-2 ${
-                enabled ? 'bg-gray-300' : 'bg-white'
-            } hover:bg-gray-100 text-gray-800 font-semibold py-2 px-2 border border-gray-400 rounded shadow inline-flex items-center`}
+                enabled ? 'bg-gray-300' : 'bg-white hover:bg-gray-100'
+            } text-gray-800 font-semibold py-2 px-2 border border-gray-400 rounded shadow inline-flex items-center`}
             onClick={onClick}
         >
             {children}
