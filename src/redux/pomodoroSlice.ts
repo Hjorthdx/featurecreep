5cbb5e7c00f6edefd85a89d2cf12849676f0e5c5
Hjorth <sboels18@student.aau.dk>
2022-07-24 15:34:27 +0200

    Updated redux pomodoro slice. Not needed after all. Needs to be updated else vercel cant deploy

--- src/redux/pomodoroSlice.ts
+++ src/redux/pomodoroSlice.ts
@@ -1,22 +1,12 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { Task } from 'prisma/prisma-client';
 
-export interface PomodoroState {
-    completedTasks: Task[];
-}
+export interface PomodoroState {}
 
 export const pomodoroSlice = createSlice({
     name: 'Pomodoro slice',
     initialState: { completedTasks: [] } as PomodoroState,
-    reducers: {
-        addCompletedTask(state: PomodoroState, { payload: completedTask }: PayloadAction<Task>) {
-            state.completedTasks.push(completedTask);
-        },
-        removeCompletedTask(state: PomodoroState, { payload: completedTask }: PayloadAction<Task>) {
-            state.completedTasks = state.completedTasks.filter((task) => task.id !== completedTask.id);
-        },
-    },
+    reducers: {},
 });
 
 export const PomodoroSliceReducer = pomodoroSlice.reducer;
-export const { addCompletedTask, removeCompletedTask } = pomodoroSlice.actions;
+export const {} = pomodoroSlice.actions;
